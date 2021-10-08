2897871f067c79616c2414dc32d61fd555dfad08
JSONRete <jsonrete@gmail.com>
2021-10-08 15:06:54 -0400

    commit tree 381ba350abd0864e68fc83ca17e2fa06c09c0b18

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,26 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+
+class Simplecomponent extends Component {
+ 
+        state = {
+          mood: 'happy'
+        }
+
+    handleClick = () => {
+        let moody = this.state.mood === 'happy' ? 'sad' : 'happy'
+        this.setState({ mood: moody })
+    }
+    
+    render() {
+        return (
+            <div onClick={this.handleClick}>
+
+            {this.state.mood}
+                
+            </div>
+        );
+    }
+}
+
+export default Simplecomponent;
